a03a4a3673f7f7c75c820a417219bef5c49582c3
Dobi-Vanish <ilopi567@mail.ru>
2025-01-21 21:39:34 +0300

    Added registration and users route, to users added necessary access token.

--- auth-service/cmd/api/routes.go
+++ auth-service/cmd/api/routes.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
@@ -21,8 +20,13 @@ func (app *Config) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Group(func(r chi.Router) {
+		r.Use(app.authTokenMiddleware("some_secret_key")) //
+
+		r.Get("/users", app.GetAllUsers)
+	})
 
 	mux.Post("/authenticate", app.Authenticate)
+	mux.Post("/registrate", app.Registrate)
 	return mux
 }
